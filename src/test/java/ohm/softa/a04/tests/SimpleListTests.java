42dd516999a7d5a8583c0c2afb39b2203398df4a
Daniel <nobodi991@gmail.com>
2023-04-12 12:01:29 +0200

    Changed everything to use generics

--- src/test/java/ohm/softa/a04/tests/SimpleListTests.java
+++ src/test/java/ohm/softa/a04/tests/SimpleListTests.java
@@ -18,11 +18,11 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 public class SimpleListTests {
 
 	private final Logger logger = LogManager.getLogger();
-	private SimpleList testList;
+	private SimpleList<Integer> testList;
 
 	@BeforeEach
 	void setup(){
-		testList = new SimpleListImpl();
+		testList = new SimpleListImpl<>();
 
 		testList.add(1);
 		testList.add(2);
@@ -50,7 +50,7 @@ public class SimpleListTests {
 	@Test
 	void testFilterAnonymousClass(){
 		logger.info("Testing the filter possibilities by filtering for all elements greater 2");
-		SimpleList result = testList.filter(new SimpleFilter() {
+		SimpleList<Integer> result = testList.filter(new SimpleFilter() {
 			@Override
 			public boolean include(Object item) {
 				int current = (int)item;
@@ -67,7 +67,7 @@ public class SimpleListTests {
 	@Test
 	void testFilterLambda(){
 		logger.info("Testing the filter possibilities by filtering for all elements which are dividable by 2");
-		SimpleList result = testList.filter(o -> ((int) o) % 2 == 0);
+		SimpleList<Integer> result = testList.filter(o -> ((int) o) % 2 == 0);
 		for(Object o : result){
 			int i = (int)o;
 			assertTrue(i % 2 == 0);
